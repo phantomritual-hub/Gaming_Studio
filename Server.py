7da94983ef96a76d3fb10f711e2a51aec0ee8c51
phantomritual-hub <phantomritual@gmail.com>
2026-02-10 18:43:42 +0530

    test Server.py

--- Server.py
+++ Server.py
@@ -1,35 +1,6 @@
 from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-
-# Temporary in-memory leaderboard
-leaderboard = {}
-
-# Root route (for testing)
-@app.route("/")
-def home():
-    return "congrats yessssssssssssssssssssssssssssssssss"
-
-# Submit score
-@app.route("/submit", methods=["POST"])
-def submit():
-    data = request.json
-    leaderboard[data[0]]=data[1]
-    return jsonify({"status": "ok"})
-
-# Get leaderboard
-@app.route("/leaderboard")
-def get_leaderboard():
-    return jsonify(leaderboard)
-
-@app.route("/delete", methods=["POST"])
-def delete():
-    data = request.json
-    exists = "can't find"
-    if data in leaderboard:
-        exists="removed " + str(data)
-        leaderboard[data]= None
-    return exists
+import requests
+import os
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
