20a9635054f44c67714629858d293f4be2e91a29
phantomritual-hub <phantomritual@gmail.com>
2026-02-17 15:26:48 +0530

    Update Server.py

--- Server.py
+++ Server.py
@@ -142,7 +142,7 @@ def show(name):
         return Response(code, mimetype="text/plain")
     if name == 'Special' and visitor == False:  
         visitor = True
-        return 'U can clearly read that it is in development, And I wonder what that means.\n Thank god that I have alr setup some security measures so that no phyco can access this' 
+        return 'U can clearly read that it is in development, And I wonder what that means.','Thank god that I have alr setup some security measures so that no phyco can access this' 
     elif name == 'Special' and visitor == True:
         return ''
     massage='hello '+name
